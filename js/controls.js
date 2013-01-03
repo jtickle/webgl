5c3c50acbfac08cfed2d1e975c719a1ad21564a0
Jeff Tickle <jeff@jefftickle.com>
2013-01-03 13:53:45 +0000

    Added what I have worked on so far

---
+++ js/controls.js
@@ -0,0 +1,140 @@
+/**
+ * @author jtickle / http://jefftickle.com
+ *
+ * NOTE: This kind of sucks because unfortunately, there is no support
+ * for "capturing" the mouse and getting deltas in the way you would
+ * expect from a first person shooter type game.  Looks like some
+ * browsers are working on that, but until the API gets ironed out,
+ * we will leave this as follows:
+ *
+ *    WASD are for translation (walk forward/back, strafe)
+ *    Arrow Keys are for looking
+ *
+ * TODO: Jumping
+ */
+
+THREE.FPSControls = function(object, domElement)
+{
+    this.object = object;
+    this.target = new THREE.Vector3(0, 0, 0);
+
+    this.movementSpeed = 1.0;
+    this.lookSpeed = 0.005;
+
+    this.lookUp = false;
+    this.lookLeft = false;
+    this.lookDown = false;
+    this.lookRight = false;
+    this.moveForward = false;
+    this.moveLeft = false;
+    this.moveBackward = false;
+    this.moveRight = false;
+
+    this.lat = 0;
+    this.lon = 0;
+    this.phi = 0;
+    this.theta = 0;
+
+    this.domElement = (domElement !== undefined) ? domElement : document;
+
+    this.handleResize = function() {
+        if(this.domElement === document) {
+            this.viewHalfX = window.innerWidth / 2;
+            this.viewHalfY = window.innerHeight / 2;
+        } else {
+            this.viewHalfX = this.domElement.offsetWidth / 2;
+            this.viewHalfY = this.domElement.offsetHeight / 2;
+        }
+    };
+
+    this.onKeyDown = function(event) {
+        switch(event.keyCode) {
+            case 38: // up
+                this.lookUp = true; prevent(event); break;
+            case 37: // left
+                this.lookLeft = true; prevent(event); break;
+            case 40: // down
+                this.lookDown = true; prevent(event); break;
+            case 39: // right
+                this.lookRight = true; prevent(event); break;
+            case 87: // W
+                this.moveForward = true; prevent(event); break;
+            case 65: // A
+                this.moveLeft = true; prevent(event); break;
+            case 83: // S
+                this.moveBackward = true; prevent(event); break;
+            case 68: // D
+                this.moveRight = true; prevent(event); break;
+        }
+    };
+
+    var prevent = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    this.onKeyUp = function(event) {
+        switch(event.keyCode) {
+            case 38: // up
+                this.lookUp = false; prevent(event); break;
+            case 37: // left
+                this.lookLeft = false; prevent(event); break;
+            case 40: // down
+                this.lookDown = false; prevent(event); break;
+            case 39: // right
+                this.lookRight = false; prevent(event); break;
+            case 87: // W
+                this.moveForward = false; prevent(event); break;
+            case 65: // A
+                this.moveLeft = false; prevent(event); break;
+            case 83: // S
+                this.moveBackward = false; prevent(event); break;
+            case 68: // D
+                this.moveRight = false; prevent(event); break;
+        }
+
+    };
+                
+    this.update = function(delta) {
+        var actualMoveSpeed = 0;
+        var actualLookSpeed = 0;
+
+        actualMoveSpeed = delta * this.movementSpeed;
+
+        if(this.moveForward)  this.object.translateZ( -actualMoveSpeed);
+        if(this.moveBackward) this.object.translateZ(  actualMoveSpeed);
+        if(this.moveLeft)     this.object.translateX( -actualMoveSpeed);
+        if(this.moveRight)    this.object.translateX(  actualMoveSpeed);
+
+        var actualLookSpeed = delta * this.lookSpeed;
+
+        if(this.lookUp)    this.lat += actualLookSpeed;
+        if(this.lookDown)  this.lat -= actualLookSpeed;
+        if(this.lookRight) this.lon += actualLookSpeed;
+        if(this.lookLeft)  this.lon -= actualLookSpeed;
+
+        this.lat = Math.max(-85, Math.min(85, this.lat));
+
+        this.phi   = THREE.Math.degToRad(90 - this.lat);
+        this.theta = THREE.Math.degToRad(this.lon);
+
+        var targetPosition = this.target, position = this.object.position;
+
+        targetPosition.x = position.x + 100 * Math.sin(this.phi) * Math.cos(this.theta);
+        targetPosition.y = position.y + 100 * Math.cos(this.phi);
+        targetPosition.z = position.z + 100 * Math.sin(this.phi) * Math.sin(this.theta);
+
+        this.object.lookAt(targetPosition);
+    }
+
+    this.domElement.addEventListener('keydown', bind(this, this.onKeyDown), false);
+    this.domElement.addEventListener('keyup', bind(this, this.onKeyUp), false);
+
+    function bind(scope, fn) {
+        return function() {
+            fn.apply(scope, arguments);
+        };
+    };
+
+    this.handleResize();
+}
